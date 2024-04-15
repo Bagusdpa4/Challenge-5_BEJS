81425593f284bca77ab91d62576d11c61e1dc593
Bagus Dwi <sugab.dwi88@gmail.com>
2024-04-15 22:21:39 +0700

    Testing Get detail  Users by id

--- tests/integration/users.spec.js
+++ tests/integration/users.spec.js
@@ -2,6 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const app = require("../../app");
 const request = require("supertest");
+let user = {}
 
 describe("test POST /api/v1/users endpoint", () => {
   beforeAll(async () => {
@@ -22,7 +23,7 @@ describe("test POST /api/v1/users endpoint", () => {
       let { statusCode, body } = await request(app)
         .post("/api/v1/users")
         .send({name,email,password,identity_type,identity_number,address,});
-
+      user = body.data
       // console.log("body", body);
       expect(statusCode).toBe(201);
       expect(body).toHaveProperty("status");
@@ -107,47 +108,38 @@ describe("test GET /api/v1/users endpoint", () => {
   });
 });
 
-// describe("test GET /api/v1/users/:id endpoint", () => {
-//   test("test menampilkan detail users by id -> sukses", async () => {
-//     try {
-//       let { statusCode, body } = await request(app)
-//         .get(`/api/v1/users/${user.id}`);
-//       console.log(body, "body")
-//       expect(statusCode).toBe(200);
-//       expect(body).toHaveProperty("status");
-//       expect(body).toHaveProperty("message");
-//       expect(body).toHaveProperty("data");
-//       expect(body.data).toHaveProperty("id");
-//       expect(body.data).toHaveProperty("name");
-//       expect(body.data).toHaveProperty("email");
-//       expect(body.data).toHaveProperty("password");
-//       expect(body.data).toHaveProperty("profiles");
-//       expect(body.data.profiles).toHaveProperty("id");
-//       expect(body.data.profiles).toHaveProperty("identity_type");
-//       expect(body.data.profiles).toHaveProperty("identity_number");
-//       expect(body.data.profiles).toHaveProperty("address");
-//       expect(body.data.profiles).toHaveProperty("user_id");
-//       expect(body.data.name).toBe(name);
-//       expect(body.data.email).toBe(email);
-//       expect(body.data.password).toBe(password);
-//       expect(body.data.profiles.identity_type).toBe(identity_type);
-//       expect(body.data.profiles.identity_number).toBe(identity_number);
-//       expect(body.data.profiles.address).toBe(address);
-//     } catch (err) {
-//       expect(err).toBe("error");
-//     }
-//   });
+describe("test GET /api/v1/users/:id endpoint", () => {
+  test("test menampilkan detail users by id -> sukses", async () => {
+    try {
+      let { statusCode, body } = await request(app).get(`/api/v1/users/${user.id}`);
+      expect(statusCode).toBe(200);
+      expect(body).toHaveProperty("status");
+      expect(body).toHaveProperty("message");
+      expect(body).toHaveProperty("data");
+      expect(body.data).toHaveProperty("id");
+      expect(body.data).toHaveProperty("name");
+      expect(body.data).toHaveProperty("email");
+      expect(body.data).toHaveProperty("password");
+      expect(body.data).toHaveProperty("profiles");
+      expect(body.data.profiles).toHaveProperty("id");
+      expect(body.data.profiles).toHaveProperty("identity_type");
+      expect(body.data.profiles).toHaveProperty("identity_number");
+      expect(body.data.profiles).toHaveProperty("address");
+      expect(body.data.profiles).toHaveProperty("user_id");
+    } catch (err) {
+      throw err;
+    }
+  });
 
-//   test("test menampilkan detail users by id -> error", async () => {
-//     try {
-//       let { statusCode, body } = await request(app)
-//         .get(`/api/v1/users/${user.id}`);
-//       expect(statusCode).toBe(404);
-//       expect(body).toHaveProperty("status");
-//       expect(body).toHaveProperty("message");
-//       expect(body).toHaveProperty("data");
-//     } catch (err) {
-//       expect(err).toBe("error");
-//     }
-//   });
-// });
+  test("test menampilkan detail users by id -> error", async () => {
+    try {
+      let { statusCode, body } = await request(app).get(`/api/v1/users/${1998}`);
+      expect(statusCode).toBe(404);
+      expect(body).toHaveProperty("status");
+      expect(body).toHaveProperty("message");
+      expect(body).toHaveProperty("data");
+    } catch (err) {
+      throw err;
+    }
+  });
+});
