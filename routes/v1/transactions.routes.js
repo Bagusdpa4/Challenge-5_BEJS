6f71d6250cfb454b613aa8394396617fc92a55e4
Bagus Dwi <sugab.dwi88@gmail.com>
2024-04-21 09:26:11 +0700

    Finish Project

--- routes/v1/transactions.routes.js
+++ routes/v1/transactions.routes.js
@@ -1,11 +1,17 @@
 const router = require("express").Router();
 const {store, index, show, destroy } = require("../../controllers/v1/transactionControllers");
-const restrict = require('../../middlewares/auth.middlewares')
+// const restrict = require('../../middlewares/auth.middlewares')
 
 // API Transactions
-router.post("/transactions",restrict, store);
-router.get("/transactions",restrict, index);
-router.get("/transactions/:id",restrict, show);
-router.delete("/transactions/:id",restrict, destroy);
+router.post("/transactions", store);
+router.get("/transactions", index);
+router.get("/transactions/:id", show);
+router.delete("/transactions/:id", destroy);
+
+// API Transactions Restrict
+// router.post("/transactions",restrict, store);
+// router.get("/transactions",restrict, index);
+// router.get("/transactions/:id",restrict, show);
+// router.delete("/transactions/:id",restrict, destroy);
 
 module.exports = router;
