ee128406c1d9e3de91573be567ca336cb0ecc4d0
Bagus Dwi <sugab.dwi88@gmail.com>
2024-04-21 09:09:21 +0700

    Finish Project

--- routes/v1/accounts.routes.js
+++ routes/v1/accounts.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const {register, index, show, destroy } = require("../../controllers/v1/accountControllers");
+const restrict = require('../../middlewares/auth.middlewares')
 
 // API Bank_Account
-router.post("/accounts", register);
-router.get("/accounts", index);
-router.get("/accounts/:id", show);
-router.delete("/accounts/:id", destroy);
+router.post("/accounts",restrict, register);
+router.get("/accounts",restrict, index);
+router.get("/accounts/:id",restrict, show);
+router.delete("/accounts/:id",restrict, destroy);
 
 module.exports = router;
--- routes/v1/transactions.routes.js
+++ routes/v1/transactions.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const {store, index, show, destroy } = require("../../controllers/v1/transactionControllers");
+const restrict = require('../../middlewares/auth.middlewares')
 
 // API Transactions
-router.post("/transactions", store);
-router.get("/transactions", index);
-router.get("/transactions/:id", show);
-router.delete("/transactions/:id", destroy);
+router.post("/transactions",restrict, store);
+router.get("/transactions",restrict, index);
+router.get("/transactions/:id",restrict, show);
+router.delete("/transactions/:id",restrict, destroy);
 
 module.exports = router;
